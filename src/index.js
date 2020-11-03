d7a7f1b2dd420161b3bbfb55de139595c77eec22
Tristan Tarpley <tristan@learnwithjuni.com>
2020-11-02 21:38:39 -0600

    integrate socketio

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('hi!');
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.send('Datasets service is alive!');
+});
+
+io.on('connection', socket => {
+  console.log('a user connected', socket);
+});
+
+io.on('disconnect', socket => {
+  console.log('disconnected');
+});
+
+http.listen(3030, () => {
+  console.log('listening on *:3000');
+});
