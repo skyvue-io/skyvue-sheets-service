c3234896f14ef5b37439556318754d6341b8ad0e
Tristan Tarpley <tristan@learnwithjuni.com>
2020-12-29 21:39:36 -0600

    fix bug in sorting logic

--- src/lib/layers/applySortings.js
+++ src/lib/layers/applySortings.js
@@ -2,23 +2,22 @@ const _ = require('lodash');
 const R = require('ramda');
 const safeParseNumber = require('../../utils/safeParseNumber');
 
-const applySortings = R.curry((sortLayer, boardData) => ({
-  ...boardData,
-  rows: _.orderBy(
+const applySortings = R.curry((sortLayer, boardData) => {
+  const indecesOfSortKeys = sortLayer.map(sorting =>
+    boardData.columns.findIndex(col => col._id === sorting.key),
+  );
+
+  const keysMappedToSelectorFunctions = indecesOfSortKeys.map(index =>
+    R.pipe(R.path(['cells', index]), R.prop('value'), safeParseNumber),
+  );
+
+  const sortedRows = _.orderBy(
     boardData.rows,
-    sortLayer.map(layer => row =>
-      safeParseNumber(
-        R.prop(
-          'value',
-          R.path(
-            ['cells', R.findIndex(x => x._id === layer.key)(boardData.columns)],
-            row,
-          ),
-        ),
-      ),
-    ),
-    sortLayer.map(x => x.direction),
-  ),
-}));
+    keysMappedToSelectorFunctions,
+    R.map(R.prop('direction'), sortLayer),
+  );
+
+  return R.assoc('rows', sortedRows, boardData);
+});
 
 module.exports = applySortings;
