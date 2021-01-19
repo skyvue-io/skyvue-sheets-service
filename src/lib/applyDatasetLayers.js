ace1c1fd72680bf29284d2d87d824919ddcf5739
Tristan Tarpley <tristan@learnwithjuni.com>
2021-01-18 18:40:11 -0600

    change applySmartColumns to add columns incrementally so that column dependencies exist at compilation time

--- src/lib/applyDatasetLayers.js
+++ src/lib/applyDatasetLayers.js
@@ -19,10 +19,12 @@ const updateRowIndeces = boardData =>
 
 const smartColumns = [
   {
+    dataType: 'string',
     _id: 'a3a6c23e-aea7-4464-89af-fa354c2b8dc5',
     expression:
-      '25176d8a-8a4d-436e-9215-cd03059b3642 + a1fc2674-341d-4124-b22e-fd95f920ee2f + (100 - 3000)',
-    columnName: 'test column',
+      // '25176d8a-8a4d-436e-9215-cd03059b3642 + a1fc2674-341d-4124-b22e-fd95f920ee2f + (100 - 3000)',
+      'CONCATENATE(c27579ab-a701-46cc-830d-87533abf511e, " ", "!")',
+    value: 'test column',
   },
 ];
 
--- src/lib/layers/applySmartColumns.js
+++ src/lib/layers/applySmartColumns.js
@@ -6,6 +6,22 @@ const findColumnIndexById = require('../findColumnIndexById');
 const SyntaxError = require('../../errors/SyntaxError');
 
 const UUID_REGEX = /\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/g;
+const SPLIT_ON_COMMAS_WITH_WHITESPACE = /,\s*/;
+const EXTRACT_FROM_PARENS_REGEX = /\(([^)]+)\)/;
+const EXTRACT_FROM_QUOTES_REGEX = /"([^']+)"/;
+
+const extractFromConcatString = R.pipe(
+  R.match(EXTRACT_FROM_PARENS_REGEX),
+  R.prop(1),
+  R.split(SPLIT_ON_COMMAS_WITH_WHITESPACE),
+  R.map(word =>
+    word.charAt(0) === '"' && word.charAt(word.length - 1) === '"'
+      ? word
+      : `"${word}"`,
+  ),
+  R.map(word => (word === '" "' ? ' ' : word.match(EXTRACT_FROM_QUOTES_REGEX)?.[1])),
+  R.join(''),
+);
 
 const replaceExpWithValues = (expression, rowIndex, boardData) =>
   expression.replace(UUID_REGEX, x =>
@@ -15,11 +31,15 @@ const replaceExpWithValues = (expression, rowIndex, boardData) =>
     ),
   );
 
-const handleParsingExpression = (expression, rowIndex, colId, boardData) => {
+const handleParsingExpression = (expression, rowIndex, col, boardData) => {
   try {
-    return math.evaluate(replaceExpWithValues(expression, rowIndex, boardData));
+    const filledVariables = replaceExpWithValues(expression, rowIndex, boardData);
+    if (col.dataType === 'string') {
+      return extractFromConcatString(filledVariables);
+    }
+    return math.evaluate(filledVariables);
   } catch (e) {
-    throw new SyntaxError(colId);
+    throw new SyntaxError(col._id);
   }
 };
 
@@ -28,38 +48,41 @@ const mapIndexed = R.addIndex(R.map);
 const applySmartColumns = R.curry((layers, boardData) => {
   if (boardData.layerToggles.smartColumns === false) return boardData;
   try {
-    const columns = R.insertAll(
-      boardData.columns.length,
-      layers.map(layer => ({
-        ...layer,
-        dataType: 'number',
-        isSmartColumn: true,
-      })),
-      boardData.columns,
+    const appendSmartColumnToDataset = R.curry((smartColumn, boardData) => ({
+      ...boardData,
+      columns: [
+        ...boardData.columns,
+        {
+          ...smartColumn,
+          isSmartColumn: true,
+          dataType: smartColumn.dataType ?? 'number',
+        },
+      ],
+      rows: mapIndexed((row, index) => ({
+        ...row,
+        cells: [
+          ...row.cells,
+          {
+            _id: uuidv4(),
+            value: handleParsingExpression(
+              smartColumn.expression,
+              index,
+              smartColumn,
+              boardData,
+            ),
+          },
+        ],
+      }))(boardData.rows),
+    }));
+
+    const incrementallyAddSmartColumns = R.apply(
+      R.pipe,
+      R.map(appendSmartColumnToDataset, layers),
     );
 
-    const rows = mapIndexed((row, index) => ({
-      ...row,
-      cells: [
-        ...row.cells,
-        ...R.map(layer => ({
-          _id: uuidv4(),
-          value: handleParsingExpression(
-            layer.expression,
-            index,
-            layer._id,
-            boardData,
-          ),
-        }))(layers),
-      ],
-    }))(boardData.rows);
-
-    return {
-      ...boardData,
-      columns,
-      rows,
-    };
+    return incrementallyAddSmartColumns(boardData);
   } catch (e) {
+    console.log('applySmartColumns:', e);
     return {
       ...boardData,
       errors: [
@@ -74,5 +97,50 @@ const applySmartColumns = R.curry((layers, boardData) => {
     };
   }
 });
+
+// const applySmartColumns = R.curry((layers, boardData) => {
+//   if (boardData.layerToggles.smartColumns === false) return boardData;
+//   try {
+//     const columns = R.insertAll(
+//       boardData.columns.length,
+//       layers.map(layer => ({
+//         ...layer,
+//         dataType: layer.dataType ?? 'number',
+//         isSmartColumn: true,
+//       })),
+//       boardData.columns,
+//     );
+
+//     const rows = mapIndexed((row, index) => ({
+//       ...row,
+//       cells: [
+//         ...row.cells,
+//         ...R.map(layer => ({
+//           _id: uuidv4(),
+//           value: handleParsingExpression(layer.expression, index, layer, boardData),
+//         }))(layers),
+//       ],
+//     }))(boardData.rows);
+
+//     return {
+//       ...boardData,
+//       columns,
+//       rows,
+//     };
+//   } catch (e) {
+//     return {
+//       ...boardData,
+//       errors: [
+//         ...(boardData.errors ?? []),
+//         {
+//           section: 'smartColumns',
+//           message: e.message,
+//           type: e.type,
+//           target: e._id,
+//         },
+//       ],
+//     };
+//   }
+// });
 
 module.exports = applySmartColumns;
