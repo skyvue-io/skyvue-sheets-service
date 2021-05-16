091b321af8d0e291bb661ba2c5b400810e39b188
Tristan Tarpley <tristan@learnwithjuni.com>
2021-05-16 14:39:58 -0500

    port wasn't capitalized

--- src/index.js
+++ src/index.js
@@ -265,7 +265,7 @@ if (shouldRunMonitor && process.env.NODE_ENV === 'development') {
   }, 10000);
 }
 
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 http.listen(port, () => {
   console.log(`listening on *:${port}`);
 });
